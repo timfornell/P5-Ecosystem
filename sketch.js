cf8c87602713c867eaa3c8bd71fb51802366b0b5
Vincent <tim.fornell@hotmail.com>
2022-02-20 21:19:24 +0100

    Add input to control number of actors and a reset button

--- sketch.js
+++ sketch.js
@@ -1,13 +1,28 @@
-let actors = {
-   "flies": [],
-   "frogs": []
-};
+let actors, numFlies, numFrogs, resetButton, inputNumFlies, inputNumFrogs;
 
-let numFlies = 100;
-let numFrogs = 1;
+numFlies = 100;
+numFrogs = 1;
 
 function setup() {
    createCanvas(1200, 500);
+
+   let numFliesText = createElement("h5", "Number of flies: ");
+   numFliesText.position(10, height);
+   let numFrogsText = createElement("h5", "Number of frogs: ");
+   numFrogsText.position(10, height + 30);
+
+   inputNumFlies = createInput(numFlies);
+   inputNumFrogs = createInput(numFrogs);
+   inputNumFlies.position(numFliesText.x + 100, height + 18);
+   inputNumFrogs.position(numFrogsText.x + 105, height + 48);
+   resetButton = createButton("Reset");
+   resetButton.position(10, height + 90);
+   resetButton.mousePressed(restartSketch);
+
+   actors = {
+      "flies": [],
+      "frogs": []
+   };
 
    spawnFlies(numActors=numFlies);
    spawnFrogs(numActors=numFrogs);
@@ -40,4 +55,10 @@ function spawnFrogs(numActors) {
    for (let i = 0; i < numActors; i++) {
       actors.frogs.push(new Frog(id=i));
    }
+}
+
+function restartSketch() {
+   numFlies = inputNumFlies.value();
+   numFrogs = inputNumFrogs.value();
+   setup();
 }
